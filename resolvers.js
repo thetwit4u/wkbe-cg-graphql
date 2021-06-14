e6c77a416eb682924064cc759589b120507aacd0
David Vanheeswijck <david.vanheeswijck@wolterskluwer.com>
2021-06-14 23:05:35 +0200

    mock diagramInfo

--- resolvers.js
+++ resolvers.js
@@ -5,6 +5,25 @@ const resolvers =
         topics: async (_,{contains,limit},{dataSources}) => {
             const topicsData = await dataSources.topicAPI.searchTopics(contains,limit);
             return topicsData
+        },
+        diagramInfo:  (_,{topicIds},{dataSources}) => {
+            // const diagramInfoData = await dataSources.topicAPI.diagramInfo(contains,limit);
+            const data = [{
+                "sets": ["1"],
+                "size": 10,
+                "label":"Minimumbezoldiging"
+            },
+            {
+                "sets": ["2"],
+                "size": 10,
+                "label":"Nettoloon"
+            },
+            {
+                "sets": ["1","2"],
+                "size": 5,
+                "label":"Minimumbezoldiging & Nettoloon"
+            }]
+            return data
         }
     }
 }
